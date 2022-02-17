6731301fa211cf49489670fc24df08ad580fc866
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-02-17 03:30:57 +0000

    Completed class - all code working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,22 @@
 class ContactsController < ApplicationController
 
+  def index
+    @contacts = Contact.all
+  end
+  
   def show
     @contact = Contact.find(params["id"])
+  end
+
+  def new
+    @contact = Contact.new
+    @contact.company_id = params["company_id"]  
+  end
+
+  def create
+    @contact = Contact.new(params["contact"])
+    @contact.save
+    redirect_to "/companies/#{@contact.company_id}"
   end
 
 end
